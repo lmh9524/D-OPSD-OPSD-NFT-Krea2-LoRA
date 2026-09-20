ac24c1b955136795474dad1dccd03db2117552c2
lmh9524 <lmh9524@users.noreply.github.com>
2026-09-20 11:33:04 +0800

    Add official TDM result gallery

--- docs/comparison/app.js
+++ docs/comparison/app.js
@@ -66,13 +66,14 @@ function addImageFallback(image) {
 }
 
 renderScene("fashion");
-document.querySelectorAll(".gallery img, .video-poster img").forEach(addImageFallback);
+document.querySelectorAll(".gallery img, .tdm-shot img, .video-poster img").forEach(addImageFallback);
 
 const lightbox = document.querySelector("#lightbox");
 const lightboxImage = lightbox.querySelector("img");
-document.querySelectorAll(".gallery-item").forEach((item) => {
+document.querySelectorAll(".gallery-item, .tdm-shot").forEach((item) => {
   item.addEventListener("click", () => {
     lightboxImage.src = item.dataset.full;
+    lightboxImage.alt = item.querySelector("img")?.alt || "放大的效果图";
     lightbox.showModal();
   });
 });
